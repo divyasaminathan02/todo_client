43da3af6471a84c64670a44c09c2c2ca035d2f4e
divyasaminathan <sridivyadharshini2007@gmail.com>
2025-12-18 14:56:22 +0530

    Redirected

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Todo.css'
+import './Pages/css/Todo.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
